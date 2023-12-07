123b9bed2ace790061b0638deefd986205a16ba6
VieraJulian <julianviera2019@hotmail.com>
2023-12-07 18:46:30 -0300

    Julián - Configuración de Eureka Server

---
+++ Eureka/src/test/java/com/UserPostProject/Eureka/EurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.UserPostProject.Eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
--- Users/src/main/java/com/UserPostProject/Users/infrastructure/outputAdapter/IPostServiceAdapter.java
+++ Users/src/main/java/com/UserPostProject/Users/infrastructure/outputAdapter/IPostServiceAdapter.java
@@ -5,13 +5,12 @@ import com.UserPostProject.Users.infrastructure.outputport.IPostServicePort;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
-@FeignClient(name = "postservice", url="http://localhost:8001/posts")
+@FeignClient(name = "posts-service")
 public interface IPostServiceAdapter extends IPostServicePort {
     @Override
-    @GetMapping("/{userId}")
+    @GetMapping("/posts/{userId}")
     List<PostDTO> getUserPosts(@PathVariable String userId);
 }
